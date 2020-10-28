cfad9bc03bc343255cc824af24fa941da63b6720
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2020-10-27 18:00:04 -0700

    Remove Tumblr link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Backgrounds from '../Backgrounds/Backgrounds';
-import MoreMenu from '../MoreMenu/MoreMenu';
-import envelopeSquare from '../../images/svg/envelope-square.svg';
-import facebookOfficial from '../../images/svg/facebook-official.svg';
-import github from '../../images/svg/github.svg';
-import tumblr from '../../images/svg/tumblr.svg';
-import twitter from '../../images/svg/twitter.svg';
-import wordpress from '../../images/svg/wordpress.svg';
-import './Header.scss';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Backgrounds from "../Backgrounds/Backgrounds";
+import MoreMenu from "../MoreMenu/MoreMenu";
+import envelopeSquare from "../../images/svg/envelope-square.svg";
+import facebookOfficial from "../../images/svg/facebook-official.svg";
+import github from "../../images/svg/github.svg";
+import twitter from "../../images/svg/twitter.svg";
+import wordpress from "../../images/svg/wordpress.svg";
+import "./Header.scss";
 
 class Header extends Component {
   static propTypes = {
-    currentSection: PropTypes.number.isRequired
+    currentSection: PropTypes.number.isRequired,
   };
 
   render() {
@@ -26,22 +25,29 @@ class Header extends Component {
         <h1 className="Header__heading">JeffreyATW</h1>
         <ul className="Header__links">
           <li className="Header__link Header__link--no-mobile">
-            <a href="https://twitter.com/JeffreyATW"><img src={twitter} alt="Twitter" /></a>
+            <a href="https://twitter.com/JeffreyATW">
+              <img src={twitter} alt="Twitter" />
+            </a>
           </li>
           <li className="Header__link Header__link--no-mobile">
-            <a href="http://jeffreyatw.tumblr.com/"><img src={tumblr} alt="Tumblr" /></a>
+            <a href="https://www.facebook.com/jeffreyatw">
+              <img src={facebookOfficial} alt="Facebook" />
+            </a>
           </li>
           <li className="Header__link Header__link--no-mobile">
-            <a href="https://www.facebook.com/jeffreyatw"><img src={facebookOfficial} alt="Facebook" /></a>
+            <a href="https://github.com/JeffreyATW">
+              <img src={github} alt="GitHub" />
+            </a>
           </li>
           <li className="Header__link Header__link--no-mobile">
-            <a href="https://github.com/JeffreyATW"><img src={github} alt="GitHub" /></a>
+            <a href="http://jeffreyatw.com/blog">
+              <img src={wordpress} alt="Blog" />
+            </a>
           </li>
           <li className="Header__link Header__link--no-mobile">
-            <a href="http://jeffreyatw.com/blog"><img src={wordpress} alt="Blog" /></a>
-          </li>
-          <li className="Header__link Header__link--no-mobile">
-            <a href="mailto:jeffreyatw@gmail.com"><img src={envelopeSquare} alt="Email" /></a>
+            <a href="mailto:jeffreyatw@gmail.com">
+              <img src={envelopeSquare} alt="Email" />
+            </a>
           </li>
           <li className="Header__link">
             <MoreMenu currentSection={currentSection} />
