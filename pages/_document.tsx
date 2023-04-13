fb8a18519997bec76a997923d20ab4b84dcede56
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2023-04-13 10:32:17 -0700

    Change font from serif to Libre Franklin

---
+++ pages/_document.tsx
@@ -0,0 +1,45 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta content="Jeffrey Carl Faden's personal website." name="Description" />
+        <meta content="Jeffrey Carl Faden" name="author" />
+        <meta
+          content="JeffreyATW, web development, front-end, blog, links"
+          name="Keywords"
+        />
+        <link
+          href="//jeffreyatw.com/feed"
+          rel="alternate"
+          title="RSS"
+          type="application/rss+xml"
+        />
+        <link
+          href="//jeffreyatw.com/blog/feed/atom"
+          rel="alternate"
+          title="Atom"
+          type="application/atom+xml"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin&display=swap" rel="stylesheet"></link>
+
+        <meta property="og:title" content="JeffreyATW" />
+        <meta
+          property="og:description"
+          content="Jeffrey Carl Faden's personal website."
+        />
+        <meta property="og:image" content="https://jeffreyatw.com/og-image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@JeffreyATW" />
+        <meta name="twitter:creator" content="@JeffreyATW" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,41 +7,12 @@ export default function Home() {
   const [currentSection, setCurrentSection] = useState(0);
 
   return (
-    <div>
+    <>
       <Head>
-        <meta content="Jeffrey Carl Faden's personal website." name="Description" />
-        <meta content="Jeffrey Carl Faden" name="author" />
-        <meta
-          content="JeffreyATW, web development, front-end, blog, links"
-          name="Keywords"
-        />
-        <link
-          href="//jeffreyatw.com/feed"
-          rel="alternate"
-          title="RSS"
-          type="application/rss+xml"
-        />
-        <link
-          href="//jeffreyatw.com/blog/feed/atom"
-          rel="alternate"
-          title="Atom"
-          type="application/atom+xml"
-        />
-
-        <meta property="og:title" content="JeffreyATW" />
-        <meta
-          property="og:description"
-          content="Jeffrey Carl Faden's personal website."
-        />
-        <meta property="og:image" content="https://jeffreyatw.com/og-image.jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@JeffreyATW" />
-        <meta name="twitter:creator" content="@JeffreyATW" />
-
         <title>JeffreyATW</title>
       </Head>
       <Header currentSection={currentSection} />
       <Content currentSection={currentSection} setSection={setCurrentSection} />
-    </div>
+    </>
   )
 }
